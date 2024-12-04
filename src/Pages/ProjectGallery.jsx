533512d21b1f32baa6de2220d85386172e26c859
Dane Cronin <danecronin@gmail.com>
2024-12-03 21:40:50 -0700

    added project info to pages.jsx and image files

--- src/Pages/ProjectGallery.jsx
+++ src/Pages/ProjectGallery.jsx
@@ -1,1 +1,37 @@
 
+import styles from './index.css';
+
+export default function ProjectGallery() {
+
+    const projects = [
+        {
+        title: "C2 Montreal",
+        src: "c2montreal.png",
+        color: "#000"
+         },
+         {
+            title: "Office Studio",
+            src: "officestudio.png",
+            color: "#8C8C8C"
+             },
+             {
+                title: "Locomotive",
+                src: "locomotive.png",
+                color: "#EFE8D3"
+                 },
+                 {
+                    title: "Silencio",
+                    src: "silencio.png",
+                    color: "#706D63"
+                     }
+    ]
+
+
+
+
+return (
+    <main className={styles.main}>
+
+    </main>
+)
+}
